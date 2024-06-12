50465334e8c3ed189ae63f5bf3c07dda1660617a
Marcel Hoene <126624078+marcelhoene@users.noreply.github.com>
2024-06-12 15:43:12 +0200

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,6 +2,6 @@ import streamlit as st
 import os
 
 # Everything is accessible via the st.secrets dict:
-st.write("DB username:", st.secrets.db_username
-st.write("DB password:", st.secrets.db_password
+st.write("DB username:", st.secrets.db_username)
+st.write("DB password:", st.secrets.db_password)
 
