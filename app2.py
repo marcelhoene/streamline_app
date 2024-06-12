989762af9b37eef9121bad49d3215c242e162f80
Marcel Hoene <126624078+marcelhoene@users.noreply.github.com>
2024-06-12 14:40:30 +0200

    nneuer file

--- app2.py
+++ app2.py
@@ -1,34 +1,8 @@
-import streamlit as st
-import fitz  # PyMuPDF
-from openai import OpenAI
-from dotenv import load_dotenv
-import os
-
-
-
+from pypdf import PdfReader
 import streamlit as st
 
-if 'upload_field_count' not in st.session_state:
-    st.session_state.upload_field_count = 1
-
-if "files" not in st.session_state:
-    st.session_state.files = [None] * st.session_state.upload_field_count
-
-## Streamlit App ###
+reader = PdfReader("/Users/marcelhoene/Python/Bewerbungsbot/Lebenslauf komplett .pdf")
+page = reader.pages[0]
 
 
-
-st.header("Upload all your files")
-
-new_files = False
-
-for i in range(st.session_state.upload_field_count):
-    upload = st.file_uploader(label="Laden Sie hier Ihre Dokumente hoch", key=i)
-    if upload is not None and st.session_state.files[i] is None:
-        st.session_state.files[i] = upload
-        new_files = True
-
-if new_files:
-    st.session_state.upload_field_count += 1
-    st.session_state.files.append(None)
-    st.rerun
+st.write(page.extract_text())
